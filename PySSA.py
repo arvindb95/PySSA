Corrected bug in calculation of F3 and updated plotting

PySSA.py
@@ -83,10 +83,10 @@ def calc_F_2(x,calc_F,p):
         F_2_x = np.zeros(len(x))
         for i,x_i in enumerate(x):
             fy = lambda y: calc_F(y) * (y**((p-2.0)/2.0))
-            if (x_i < 22778.5451769):
+            if (x_i < 20000):
                 F_2_x[i] = (np.sqrt(3) * integrate.quad(fy,0,x_i)[0])
             else:
-                F_2_x[i:] = (np.sqrt(3) * integrate.quad(fy,0,22778.5451769)[0]) 
+                F_2_x[i:] = (np.sqrt(3) * integrate.quad(fy,0,20000)[0]) 
         return F_2_x
 
 def calc_F_3(x,calc_F,p):
@@ -101,10 +101,10 @@ def calc_F_3(x,calc_F,p):
         F_3_x = np.zeros(len(x))
         for i,x_i in enumerate(x):
             fy = lambda y: calc_F(y) * (y**((p-3.0)/2.0))
-            if (x_i > 22778.5451769):
+            if (x_i < 2000):
                 F_3_x[i] = (np.sqrt(3) * integrate.quad(fy,0,x_i)[0])
             else:
-                F_3_x[i:] = (np.sqrt(3) * integrate.quad(fy,0,22778.5451769)[0]) 
+                F_3_x[i:] = (np.sqrt(3) * integrate.quad(fy,0,2000)[0]) 
         return F_3_x
 
 #--------------------------------------------------------------
@@ -194,9 +194,13 @@ def SSA_flux_density(t,t_0,nu,d,eta,B_0,r_0,alpha_r,p,nu_m_0,s,xi,scriptF_0,alph
     C_f = calc_C_f(B_0,10**(log_r_0),d,p)
 
     nu_m = calc_nu_m(t,10**(log_nu_m_0),t_0,alpha_gamma,alpha_B)
+    print("nu_m : ",nu_m)
     x = (2.0/3.0) * (nu/nu_m)
+    print("x : ",x)
     F2 = calc_F_2(x,calc_F,p)
+    print("F2 : ",F2)
     F3 = calc_F_3(x,calc_F,p)
+    print("F3 : ",F3)
 
     tau_nu = calc_tau_nu(t,t_0,C_tau,alpha_r,alpha_gamma,alpha_B,alpha_scriptF,p,nu,F2)
     f_nu = calc_f_nu(t,t_0,C_f,alpha_r,alpha_B,tau_nu,xi,p,nu,F2,F3)

plotter.py
@@ -29,16 +29,16 @@ alpha_r = 0.9
 p = 3.0
 nu_m_0 = 0.02e9              # Hz
 log_nu_m_0 = np.log10(nu_m_0)
-s = 1.0
-xi = 0.9
+s = 2.0
+xi = 1.0
 
 scriptF_0 = 1.0                   # as we have eps_e = eps_B
 alpha_scriptF = 0.0               # as we have eps_e = eps_B at all times
 
 
-#guess_parameters = B_0, np.log10(r_0)#, alpha_r, p, np.log10(nu_m_0), s, xi
+guess_parameters = B_0, np.log10(r_0)#, alpha_r, p, np.log10(nu_m_0), s, xi
 
-guess_parameters = B_0, s, xi
+#guess_parameters = B_0, s, xi
 
 scriptF_0 = 1.0                   # as we have eps_e = eps_B
 alpha_scrpitF = 0.0               # as we have eps_e = eps_B at all times
@@ -50,7 +50,7 @@ e = (const.e.esu).value                   # esu
 c = (const.c.cgs).value                   # cm/s
 
 
-filename="later_times_chain.h5"
+filename="first_100_days_chain.h5"
 
 reader = emcee.backends.HDFBackend(filename)
 
@@ -67,19 +67,21 @@ print("flat chain shape: {0}".format(samples.shape))
 #print("flat log prob shape: {0}".format(log_prob_samples.shape))
 #print("flat log prior shape: {0}".format(log_prior_samples.shape))
 
-ndim = 3
+#ndim = 3
+ndim = 2
 
 B_best_fit = np.percentile(samples[:,0], [16,50,84])
-s_best_fit = np.percentile(samples[:,1], [16,50,84])
-xi_best_fit = np.percentile(samples[:,2], [16,50,84])
+s_best_fit = 10**(np.percentile(samples[:,1], [16,50,84]))
+#xi_best_fit = np.percentile(samples[:,2], [16,50,84])
 print(B_best_fit)
 print(s_best_fit)
-print(xi_best_fit)
+#print(xi_best_fit)
 
 print(len(samples[:,0]))
 
 #fig, axes = plt.subplots(ndim, figsize=(10, 14), sharex=True)
-labels=[r"$B_{{0}}$",r"$s$",r"$\xi$"]
+#labels=[r"$B_{{0}}$",r"$s$",r"$\xi$"]
+labels=[r"$B_{{0}}$",r"$log_{{10}}r_{{0}}$"]
 #for i in range(ndim):
 #    ax = axes[i]
 #    ax.plot(samples[:, i], "C0", alpha=0.7)
@@ -94,30 +96,47 @@ labels=[r"$B_{{0}}$",r"$s$",r"$\xi$"]
 
 c = ChainConsumer()
 c.add_chain(samples, parameters=labels)
+c.configure(colors=["#ff7f0e"], bar_shade=True)
 #c.configure(statistics="cumulative")
 
 B_best_fit = c.analysis.get_summary()[labels[0]][1]
-s_best_fit = c.analysis.get_summary()[labels[1]][1]
-xi_best_fit = c.analysis.get_summary()[labels[2]][1]
+r_best_fit = c.analysis.get_summary()[labels[1]][1]
+#s_best_fit = c.analysis.get_summary()[labels[1]][1]
+#xi_best_fit = c.analysis.get_summary()[labels[2]][1]
 
-fig2 = c.plotter.plot(figsize=(8,7), truth=[B_best_fit,s_best_fit,xi_best_fit])
+print("B best fit : ",B_best_fit)
+print("r best fit : ",10**r_best_fit)
+
+#fig2 = c.plotter.plot(figsize=(8,7), truth=[B_best_fit,s_best_fit,xi_best_fit])
+
+fig2 = c.plotter.plot(figsize=(8,7), truth=[B_best_fit,r_best_fit])
 
 plt.savefig("corner_test_later_timesi_maxlik.pdf")
 
-data_table = Table.read("SN2004dk_final_data_later_times.txt",format="ascii")
+data_table = Table.read("SN2004dk_final_data_100days.txt",format="ascii")
 t_obs = data_table['col1'].data
 t_0 = 10
 nu = data_table['col2'].data * 10**9       ### Make sure this is in Hz
 F = data_table['col3'].data / 1000         ### Make sure this is in mJy
 F_err = data_table['col4'].data /1000      ### Make sure this is in mJy
 
-t = np.logspace(2,4,100)
+#t = np.logspace(5,6,100)
 
-ssa_fnu = SSA_flux_density(t,t_0,3e9,d,eta,B_best_fit,r_0,alpha_r,p,nu_m_0,s_best_fit,xi_best_fit,scriptF_0,alpha_scriptF)
+t = np.logspace(0.5, 3, 100)
+
+ssa_fnu = SSA_flux_density(t,t_0,3e9,d,eta,B_best_fit,10**(r_best_fit),alpha_r,p,nu_m_0,s,xi,scriptF_0,alpha_scriptF)
 
 fig3 = plt.figure()
 
-plt.plot(t,ssa_fnu)
+plt.plot(t,ssa_fnu,c="C1")
 plt.yscale("log")
 plt.xscale("log")
-plt.show()
+plt.savefig("first_100days_lc.pdf")
+
+def calc_chi_sq(obs_flux,obs_flux_err,obs_time,obs_freq,t_0,d,eta,B_0,r_0,alpha_r,p,nu_m_0,s,xi,scriptF_0,alpha_scriptF):
+    model_flux = SSA_flux_density(obs_time,t_0,obs_freq,d,eta,B_0,r_0,alpha_r,p,nu_m_0,s,xi,scriptF_0,alpha_scriptF)
+    chi_sq = np.sum((obs_flux-model_flux)**2/(obs_flux_err**2))
+    return chi_sq
+
+
+print("Chi_sq : ",calc_chi_sq(F,F_err,t_obs,nu,t_0,d,eta,B_best_fit,10**(r_best_fit),alpha_r,p,nu_m_0,s,xi,scriptF_0,alpha_scriptF))