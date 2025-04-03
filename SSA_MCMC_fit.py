4b131e74c3b9a4fa8c4320d9a395d1c47b21759c
Arvind Balasubramanian <arvind6895@gmail.com>
2025-04-03 11:37:53 +0530

    Modified MCMC code to be more automatic with dictionary inputs

--- SSA_MCMC_fit.py
+++ SSA_MCMC_fit.py
@@ -12,7 +12,6 @@ import matplotlib.pyplot as plt
 from multiprocessing import Pool
 import matplotlib as mpl
 from chainconsumer import ChainConsumer
-import os
 
 params = {"font.family": "serif", "text.usetex": True}
 
@@ -21,38 +20,36 @@ mpl.rcParams.update(params)
 ## ---------------------- Begin Code ---------------------- ##
 
 ## Main physical parameters ##
-params = {
+
+# Fixed params
+
+fixed_params = {
     "d": ((92.0 * u.Mpc).to(u.cm)).value,  # cm ; distance to source
     "t_0": 10,  # reference time 10 days since explosion
     "eta": 10,  # shell radius to thickness factor
-    "B_0": 4.5,  # G
-    "r_0": 4.3e15,  # cm
-    "alpha_r": 0.96,
     "p": 3.2,
     "nu_m_0": 1e9,  # Hz
     "s": 2.0,
-    "xi": 0.5,
     "scriptF_0": 1.0,  # as we have eps_e = eps_B
     "alpha_scriptF": 0.0,  # as we have eps_e = eps_B at all times
 }
 
-# guess_parameters = B_0, np.log10(r_0)#, alpha_r, p, np.log10(nu_m_0), s, xi
+# Variable parameters
 
-guess_parameters = (
-    params["B_0"],
-    np.log10(params["r_0"]),
-    params["alpha_r"],
-    params["xi"],
-)
+var_params = {
+    "B_0": 4.5,  # G
+    "r_0": 4.3e15,  # cm
+    "alpha_r": 0.96,
+    "xi": 0.5,
+}
 
-d = params["d"]
-t_0 = params["t_0"]
-eta = params["eta"]
-p = params["p"]
-nu_m_0 = params["nu_m_0"]
-s = params["s"]
-scriptF_0 = params["scriptF_0"]
-alpha_scriptF = params["alpha_scriptF"]
+var_params_names = list(var_params.keys())
+print(var_params_names)
+param_scale_log = [False, True, False, False]
+bounds = [(1.0e-50, np.inf), (1.0e-50, np.inf), (1.0e-50, 1.0), (0.0, 1.0)]
+
+guess_params = np.array(list(var_params.values()))
+guess_params[param_scale_log] = np.log10(guess_params[param_scale_log])
 
 ## Physical constants ##
 
@@ -74,68 +71,48 @@ fluxerrs = comp_data["Fluxerr"].data
 
 
 def lnprior(theta):
-    B_0, log_r_0, alpha_r, xi = theta
+    allow_param_set = True
 
-    if (
-        (B_0 >= 1.0e-50)
-        and (10 ** (log_r_0) >= 1.0e-50)
-        and (1.0e-50 <= alpha_r <= 1.0)
-        and (0.0 <= xi <= 1.0)
-    ):
+    for param_id in range(len(theta)):
+        allow_param_set = allow_param_set and (
+            bounds[param_id][0] <= theta[param_id] <= bounds[param_id][1]
+        )
+    if allow_param_set:
         return 0.0
     else:
         return -np.inf
 
 
-def lnlike(theta, t, t_0, nu, F_obs, F_err):
-    B_0, log_r_0, alpha_r, xi = theta
+def lnlike(theta, t, nu, F_obs, F_err):
+    new_var_param_dict = {}
 
-    f_nu = SSA_flux_density(
-        t,
-        nu,
-        d,
-        t_0,
-        eta,
-        B_0,
-        10 ** (log_r_0),
-        alpha_r,
-        p,
-        nu_m_0,
-        s,
-        xi,
-        scriptF_0,
-        alpha_scriptF,
-    )
+    for var_i in range(len(var_params_names)):
+        if param_scale_log[var_i]:
+            new_var_param_dict.update({var_params_names[var_i]: 10 ** (theta[var_i])})
+        else:
+            new_var_param_dict.update({var_params_names[var_i]: theta[var_i]})
 
+    f_nu = SSA_flux_density(t, nu, **new_var_param_dict, **fixed_params)
     inv_sigma2 = 1.0 / F_err**2.0
-
-    new_tab = Table(
-        [theta, f_nu],
-        names=["theta", "f_nu"],
-    )
-    new_tab.write("store_flux_values.txt", format="ascii")
 
     return -0.5 * (np.sum((F_obs - f_nu) ** 2 * inv_sigma2 - np.log(inv_sigma2)))
 
 
-def lnprob(theta, t, t_0, nu, F_obs, F_err):
+def lnprob(theta, t, nu, F_obs, F_err):
     lp = lnprior(theta)
 
     if not np.isfinite(lp):
         return -np.inf
 
-    return lp + lnlike(theta, t, t_0, nu, F_obs, F_err)
+    return lp + lnlike(theta, t, nu, F_obs, F_err)
 
 
 def get_starting_pos(guess_parameters, nwalkers, ndim=7):
-    B_0, log_r_0, alpha_r, xi = guess_parameters
-
-    # pos = [np.asarray([B_0, log_r_0, alpha_r, p, log_nu_m_0, s, xi]) + 1e-4*np.random.randn(ndim) for i in range(nwalkers)]
-    # pos = [np.asarray([B_0, log_r_0]) + 1e-4*np.random.randn(ndim) for i in range(nwalkers)]
     pos = [
-        np.asarray([B_0, log_r_0, alpha_r, xi]) + 1e-4 * np.random.randn(ndim)
+        np.asarray(list(guess_params)) + 1e-4 * np.random.randn(ndim)
         for i in range(nwalkers)
     ]
+
     return pos
 
 
@@ -143,7 +120,6 @@ def run_mcmc(
     guess_parameters,
     pool,
     backend_file,
-    t_0=10,
     niters=500,
     nwalkers=200,
     ndim=7,
@@ -163,7 +139,7 @@ def run_mcmc(
             nwalkers,
             ndim,
             lnprob,
-            args=(t, t_0, nu, F, F_err),
+            args=(t, nu, F, F_err),
             backend=backend,
             pool=pool,
         )
@@ -183,7 +159,7 @@ def run_mcmc(
             nwalkers,
             ndim,
             lnprob,
-            args=(t, t_0, nu, F, F_err),
+            args=(t, nu, F, F_err),
             backend=backend,
             pool=pool,
         )
@@ -222,13 +198,13 @@ def run_mcmc(
 # print(better_guess_params["x"])
 # print(better_guess_params)
 
-with Pool() as pool:
-    sampler = run_mcmc(
-        [4.473e00, 1.568e01, 9.543e-01, 3.808e-01],
-        niters=10,
-        nwalkers=10,
-        ndim=4,
-        pool=pool,
-        backend_file="SN2003L.h5",
-        restart=False,
-    )
+# with Pool() as pool:
+#    sampler = run_mcmc(
+#        [4.473e00, 1.568e01, 9.543e-01, 3.808e-01],
+#        niters=10,
+#        nwalkers=10,
+#        ndim=4,
+#        pool=pool,
+#        backend_file="SN2003L.h5",
+#        restart=False,
+#    )
