128ebdaab36c6a0362e12b2ff1e204e1c182157d
Arvind Balasubramanian <arvind6895@gmail.com>
2025-08-24 09:10:14 +0530

    Converted txt files for values to pickle files for fast loading

--- Interpolation_files/read_files.py
+++ Interpolation_files/read_files.py
@@ -1,14 +1,39 @@
-# from astropy.table import Table
-#
-# tab = Table.read("F_values.txt", format="ascii")
-#
-# x = tab["x"].data
-# F = tab["F"].data
-#
-# print(len(x))
+import time
+from astropy.table import Table
+import pickle
 
-from pandas import read_fwf
+ast_time_start = time.time()
 
-t = read_fwf("F2_values.txt")
+tab = Table.read("F3_values.txt", format="ascii")
 
-print(t)
+x = tab["x"].data
+p = tab["p"].data
+F3 = tab["F3"].data
+
+ast_time_end = time.time()
+
+print("Time taken to load file using astropy is = ", ast_time_end - ast_time_start)
+
+F_dict = {"x": x, "p": p, "F3": F3}
+
+filehandler = open("F3_values.pkl", "wb")
+
+pickle.dump(F_dict, filehandler)
+
+filehandler.close()
+
+"""
+pkl_time_start = time.time()
+
+filehandler = open("F2_values.pkl", "rb")
+
+F_dict = pickle.load(filehandler)
+
+filehandler.close()
+
+pkl_time_end = time.time()
+
+print(F_dict)
+
+print("Time taken to load using pickle = ", pkl_time_end - pkl_time_start)
+"""
