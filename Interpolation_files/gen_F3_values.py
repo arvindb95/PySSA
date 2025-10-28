7666b7eb7b779e1b224a1578a18703ef8c5d006c
Arvind Balasubramanian <arvind6895@gmail.com>
2025-10-28 11:09:56 +0530

    Trying to change the interpolation pickle files

--- Interpolation_files/gen_F3_values.py
+++ Interpolation_files/gen_F3_values.py
@@ -2,7 +2,6 @@ import numpy as np
 from scipy import integrate, special
 from astropy.table import Table
 from tqdm import tqdm
-import pickle
 
 
 def calc_F(x):
@@ -23,45 +22,42 @@ def calc_F(x):
         return F_x
 
 
-def calc_F_2(x, calc_F, p):
+def calc_F_3(x, calc_F, p):
     """
-    Returns values of function F2 (defined in eq. A7 of Soderberg et al. 2005)
+    Returns values of function F3 (defined in eq. A7 of Soderberg et al. 2005)
     at x
     """
 
-    def fy2(y):
-        return calc_F(y) * (y ** ((p - 2.0) / 2.0))
+    def fy3(y):
+        return calc_F(y) * (y ** ((p - 3.0) / 2.0))
 
     if isinstance(x, float):
-        return np.sqrt(3) * integrate.quad(fy2, 0, x)[0]
+        return np.sqrt(3) * integrate.quad(fy3, 0, x)[0]
     else:
-        F_2_x = np.zeros(len(x))
+        F_3_x = np.zeros(len(x))
         for i, x_i in enumerate(x):
-            if x_i < 20000:
-                F_2_x[i] = np.sqrt(3) * integrate.quad(fy2, 0, x_i)[0]
+            if x_i < 2000:
+                F_3_x[i] = np.sqrt(3) * integrate.quad(fy3, 0, x_i)[0]
             else:
-                F_2_x[i:] = np.sqrt(3) * integrate.quad(fy2, 0, 20000)[0]
-        return F_2_x
+                F_3_x[i:] = np.sqrt(3) * integrate.quad(fy3, 0, 2000)[0]
+        return F_3_x
 
 
 x1 = np.arange(0, 20, 1e-6)
 x2 = np.arange(20, 700, 0.01)
 x = np.append(x1, x2)
 
-print(x)
-
 p = np.arange(2, 3.5, 1e-6)
 
-F2 = []
+F3 = []
 
 for i in tqdm(range(len(x))):
     for j in tqdm(range(len(p))):
-        F2.append(calc_F_2(x[i], calc_F, p[j]))
+        F3.append(calc_F_3(x[i], calc_F, p[j]))
 
-val_dict = {"x": x, "p": p, "F2": F2}
+val_dict = {"x": x, "p": p, "F3": F3}
 
 
-with open("F2_values.pkl", "wb") as f:
+with open("F3_values.pkl", "wb") as f:
     pickle.dump(val_dict, f)
 
-# tab = Table([x, p, F2], names=["x", "p", "F2"])
